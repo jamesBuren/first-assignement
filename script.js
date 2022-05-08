a4b885bba56fda13ccc669470baf3d9fa23f55b0
jamesBuren <jr.buren@gmail.com>
2022-05-08 13:56:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const balance=50000;  
+
+function withdraw(amount)   //this function should be called accept not withdraw 
+ { //you didn't close this brace
+
+  var sure = confirm("Are You Sure You Want To Withdraw This Amount?");
+  if(true)  //should be if (sure)
+      {
+        balance = balance - amount;
+      }else{
+        alert("No Withdrawal Made");
+      }
+       alert("Your Balance Is "+balance);
+    }
